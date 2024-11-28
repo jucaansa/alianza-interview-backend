c1c8d39edd395201328a90de98dec879a50bb126
Juan Andrade <cnadrade980318@gmail.com>
2024-11-28 15:48:24 -0500

    first commit

---
+++ src/main/java/com/juan_andrade/alianza_interview/AlianzaInterviewApplication.java
@@ -0,0 +1,13 @@
+package com.juan_andrade.alianza_interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlianzaInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlianzaInterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juan_andrade/alianza_interview/AlianzaInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juan_andrade.alianza_interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlianzaInterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
